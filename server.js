5f652fc70c8ca4da4c6b2b8ba2e0f99453f1b7d0
yunus alzahrani <yunus@yunuss-MacBook-Pro.local>
2020-01-08 02:31:37 +0300

    init server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+
+app.get('/', (req, res) => {
+    res.send('hi, Mr Client')
+})
+
+app.get('/2', (req, res) => {
+    res.send('hi, Mr Client1')
+})
+
+app.listen(3000)
